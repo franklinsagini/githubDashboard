f80c196a88281e5505dcb3acdba9774709010a4a
Franklin Sagini <franklinsagini@gmail.com>
2016-01-27 13:18:06 +0300

    first commit

---
+++ app/controllers/home.js
@@ -0,0 +1,20 @@
+angular.module('MyApp')
+  .controller('HomeCtrl', function($scope, $http) {
+      $http.jsonp('https://api.github.com/users/franklinsagini/repos')
+      .success(function(data) {
+        if (data) {
+          if (data.data) {
+            $scope.repositories = data.data;
+          }
+           if (data.data.stargazers_count) {
+            $scope.stars = data.data.stargazers_count;
+          }
+          if (data.data.forks) {
+            $scope.forks = data.data.forks;
+          }
+          if (data.data.open_issues) {
+            $scope.issues = data.data.open_issues;
+          }
+        }
+      });
+  });
